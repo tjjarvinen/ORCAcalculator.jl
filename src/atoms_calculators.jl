6c835892568b5d5b58d52fd10c0189a7a407230b
Teemu Järvinen <teemu.j.jarvinen@gmail.com>
2024-05-20 23:27:29 -0700

    add combination calculator

--- src/atoms_calculators.jl
+++ src/atoms_calculators.jl
@@ -31,3 +31,15 @@ end
 
 
 AtomsCalculators.promote_force_type(::Any, ::ORCAcalculatorbundle) = SVector(1., 1., 1.) * u"hartree/bohr" |> typeof
+
+
+function AtomsCalculators.energy_forces(
+    system, 
+    orca::ORCAcalculatorbundle; 
+    orca_stdout=stdout, 
+    numgrad=false, 
+    kwargs...
+)
+    res = calculate(system, orca.exection, orca.method; orca_stdout=orca_stdout, engrad=true, numgrad=numgrad)
+    return ( energy=res[:energy], forces=res[:forces])
+end
