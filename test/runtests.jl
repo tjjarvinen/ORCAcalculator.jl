bf837ada08e948de8835529b0c4c66cb88633e08
Teemu Järvinen <teemu.j.jarvinen@gmail.com>
2024-05-20 19:46:26 -0700

    all working

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,21 @@
 using ORCAcalculator
+using AtomsBase
+using AtomsCalculators
+using AtomsCalculators.AtomsCalculatorsTesting
+using Unitful
+using UnitfulAtomic
 using Test
 
 @testset "ORCAcalculator.jl" begin
     # Write your tests here.
+    ox = OrcaExecutable()
+    om = OrcaMethod("blyp", "def2-svp")
+    orca = ORCAcalculatorbundle( ox, om )
+
+    hydrogen = isolated_system([
+        :H => [0, 0, 0.]u"Å",
+        :H => [0, 0, 1.]u"Å"
+    ])
+
+    test_energy_forces(hydrogen, orca; orca_stdout=devnull)
 end
